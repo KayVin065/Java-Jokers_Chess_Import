89d260f7e4014eaa216273ae9b4e3d3b508ae6e3
Kayla Vincent <jqg8@txstate.edu>
2024-07-17 14:31:35 -0500

    chessKV branch before overriding

--- Jokers.java
+++ Jokers.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class Jokers 
 {
     public static void main(String[] args)
     {
+        Scanner scnr = new Scanner(System.in);
         Board GameBoard = new Board();
+        String input;
+
+        input = scnr.nextLine();
+
         /**
          * TODO: get user input()
          * TODO: translate user input in location variables
@@ -12,4 +18,43 @@ public class Jokers
          * TODO:Create a player() class
          */
     }
+    
+    public static int convert(String x) {
+        int output = 0;
+        switch(x) {
+            case "A" : 
+                return 0;
+
+            case "B" : 
+                return 1;
+            
+            case "C" : 
+                return 2;
+            
+            case "D" : 
+                return 3;
+            
+            case "E" : 
+                output = 2;
+                break;
+        
+            case "F" : 
+                output = 2;
+                break;
+            
+            case "G" : 
+                output = 2;
+                break;
+            
+            case "H" : 
+                output = 2;
+                break;
+            
+            default: 
+                System.out.println("Invalid column");
+        }
+
+        return output;
+    }
+
 }
